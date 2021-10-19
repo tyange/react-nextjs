10f092bf02b3766ce1fd67bc47dbf508dc64250a
babyazalea <usun12@hotmail.com>
2021-10-19 16:28:33 +0900

    working with dummy data & testing

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Tokyo_Skytree_2014_%E2%85%A2.jpg/668px-Tokyo_Skytree_2014_%E2%85%A2.jpg"
+      title="First Meetup"
+      address="Tokyo"
+      description="the meetup description"
+    />
+  );
+}
+
+export default MeetupDetails;
