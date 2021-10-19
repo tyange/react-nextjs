4e14c01df26af902ec8146be6bf9c188c6a45ea0
babyazalea <usun12@hotmail.com>
2021-10-19 15:34:37 +0900

    setting up new project & apply layout

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,34 @@
-// domain.com/
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "first meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Tokyo_Skytree_2014_%E2%85%A2.jpg/668px-Tokyo_Skytree_2014_%E2%85%A2.jpg",
+    address: "Sumida, Tokyo, Japan",
+    description: "Tokyo, worked before with Eunmin-Choi",
+  },
+  {
+    id: "m2",
+    title: "second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Tokyo_Skytree_2014_%E2%85%A2.jpg/668px-Tokyo_Skytree_2014_%E2%85%A2.jpg",
+    address: "Sumida, Tokyo, Japan",
+    description: "Tokyo, worked before with Eunmin-Choi",
+  },
+  {
+    id: "m3",
+    title: "third meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Tokyo_Skytree_2014_%E2%85%A2.jpg/668px-Tokyo_Skytree_2014_%E2%85%A2.jpg",
+    address: "Sumida, Tokyo, Japan",
+    description: "Tokyo, worked before with Eunmin-Choi",
+  },
+];
 
 function HomePage() {
-  return (
-    <div>
-      <h1>home page</h1>
-    </div>
-  );
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
 
 export default HomePage;
