abd5e8b4be9c687b3a888f0095368fd510d3e3bb
babyazalea <usun12@hotmail.com>
2021-10-20 00:35:20 +0900

    using api route

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,29 @@
+import { MongoClient } from "mongodb";
+
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://usun16:gchRQg1mZ5YYguec@mean-course.n7aqc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
